7489adc9c31049a9c76a8e77abcce1abdd623863
alex59493 <alexandre.peccaud@gmail.com>
2017-02-10 14:02:23 +0100

    Only load header scripts (waterline)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
 $(function(){
     /*
-        typed.js -- animated text
-    */
-    $("header h2 span").typed({
-        stringsElement: $('#typed-strings'),
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 3000,
-        showCursor: true,
-        cursorChar: "|",
-        loop: true,
-        // call when done callback function
-        callback: function() {},
-        // starting callback function before each string
-        preStringTyped: function() {},
-        //callback for every typed string
-        onStringTyped: function() {},
-        // callback for reset
-        resetCallback: function() {}
-    });
-
-
-
-    /*
         Smooth-Scrolling-JavaScript
     */
     $(".scroll").click(function(event){
