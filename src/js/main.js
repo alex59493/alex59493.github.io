0f3606dca28be36d28f100b2a7e6d09d84dd006d
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2017-09-19 15:23:12 +0200

    Refactor javascript && Complete header dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,133 +1,108 @@
-$(function(){
-    // typed.js - animated text
-    initTyped(["Je suis un développeur Web.", "Je développe vos sites Web.", "Je suis un Web designer."])
+$(function() {
+
+    // Initialise Type,js library to animate the text in the header
+    // Only initialise Type.js if the screen is larger than xs
+    var mq = window.matchMedia("(min-width: 1px) and (max-width: 576px)");
+    if (!mq.matches) {
+
+        initTypedText([
+            "Je suis un développeur Web.",
+            "Je développe vos sites Web.",
+            "Je suis un Web designer."
+        ]);
+
+    }
 
 
-
-    /* Smooth-Scrolling-JavaScript */
-        $(".scroll").click(function(event) {
-            event.preventDefault();
-            $('html,body').animate({scrollTop: $(this.hash).offset().top - 70}, 1000);
-        });
-    /* //Smooth-Scrolling-JavaScript */
+    // Smooth-Scrolling-JavaScript
+    $(".scroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop: $(this.hash).offset().top - 70}, 1000);
+    });
 
 
-
-    (function(){
+    // Initialize parallax effect for images scrolling
+    (function() {
+        var parallax = document.querySelectorAll(".parallax");
+        var speed = 0.5;
+    
+        window.onscroll = function() {
+            [].slice.call(parallax).forEach(function(el,i) {
+                var windowYOffset = window.pageYOffset;
+                var elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
         
-        var parallax = document.querySelectorAll(".parallax"),
-            speed = 0.5;
-    
-        window.onscroll = function(){
-        [].slice.call(parallax).forEach(function(el,i){
-    
-            var windowYOffset = window.pageYOffset,
-                elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-    
-            el.style.backgroundPosition = elBackgrounPos;
-    
-        });
+                el.style.backgroundPosition = elBackgrounPos;
+            });
         };
-    
-    })();
+    }) ();
 
 
     /* Waypoints (trigger event on scroll) */
-        // Skills Items
-        $('#skills .item1').css('opacity', 0);
-        $('#skills .item2').css('opacity', 0);
-        $('#skills .item3').css('opacity', 0);
-        $('#skills .item4').css('opacity', 0);
-        $('#skills .item5').css('opacity', 0);
-        $('#skills .item6').css('opacity', 0);
-        $('#skills .item7').css('opacity', 0);
-        $('#skills .item8').css('opacity', 0);
+        // // Skills Items
+        // $('#skills .item1').css('opacity', 0);
+        // $('#skills .item2').css('opacity', 0);
+        // $('#skills .item3').css('opacity', 0);
+        // $('#skills .item4').css('opacity', 0);
+        // $('#skills .item5').css('opacity', 0);
+        // $('#skills .item6').css('opacity', 0);
+        // $('#skills .item7').css('opacity', 0);
+        // $('#skills .item8').css('opacity', 0);
 
-        $("#skills .item1").waypoint(function() {
-            $('#skills .item1').addClass('animated fadeInLeft');
-        }, { offset: '100%'});
+        // $("#skills .item1").waypoint(function() {
+        //     $('#skills .item1').addClass('animated fadeInLeft');
+        // }, { offset: '100%'});
 
-        $("#skills .item2").waypoint(function() {
-            $('#skills .item2').addClass('animated fadeInRight');
-        }, { offset: '100%'});
+        // $("#skills .item2").waypoint(function() {
+        //     $('#skills .item2').addClass('animated fadeInRight');
+        // }, { offset: '100%'});
 
-        $("#skills .item3").waypoint(function() {
-            $('#skills .item3').addClass('animated fadeInLeft');
-        }, { offset: '100%'});
+        // $("#skills .item3").waypoint(function() {
+        //     $('#skills .item3').addClass('animated fadeInLeft');
+        // }, { offset: '100%'});
 
-        $("#skills .item4").waypoint(function() {
-            $('#skills .item4').addClass('animated fadeInRight');
-        }, { offset: '100%'});
+        // $("#skills .item4").waypoint(function() {
+        //     $('#skills .item4').addClass('animated fadeInRight');
+        // }, { offset: '100%'});
 
-        $("#skills .item5").waypoint(function() {
-            $('#skills .item5').addClass('animated fadeInLeft');
-        }, { offset: '100%'});
+        // $("#skills .item5").waypoint(function() {
+        //     $('#skills .item5').addClass('animated fadeInLeft');
+        // }, { offset: '100%'});
 
-        $("#skills .item6").waypoint(function() {
-            $('#skills .item6').addClass('animated fadeInRight');
-        }, { offset: '100%'});
+        // $("#skills .item6").waypoint(function() {
+        //     $('#skills .item6').addClass('animated fadeInRight');
+        // }, { offset: '100%'});
 
-        $("#skills .item7").waypoint(function() {
-            $('#skills .item7').addClass('animated fadeInLeft');
-        }, { offset: '100%'});
+        // $("#skills .item7").waypoint(function() {
+        //     $('#skills .item7').addClass('animated fadeInLeft');
+        // }, { offset: '100%'});
 
-        $("#skills .item8").waypoint(function() {
-            $('#skills .item8').addClass('animated fadeInRight');
-        }, { offset: '100%'});
-        // //Skills Items
+        // $("#skills .item8").waypoint(function() {
+        //     $('#skills .item8').addClass('animated fadeInRight');
+        // }, { offset: '100%'});
+        // // //Skills Items
 
-        // Work Items
-        $('#work .hovereffect').css('opacity', 0);
+        // // Work Items
+        // $('#work .hovereffect').css('opacity', 0);
 
-        $("#work .hovereffect").waypoint(function() {
-            $('#work .hovereffect').addClass('animated fadeIn');
-        }, { offset: '100%'});
-        // //Work Items
+        // $("#work .hovereffect").waypoint(function() {
+        //     $('#work .hovereffect').addClass('animated fadeIn');
+        // }, { offset: '100%'});
+        // // //Work Items
     /* //Waypoints */
 
 
 
     /* Contact form - Email */
-        $('#contactForm').submit(function(e){
+        $('#contactForm').submit(function(e) {
             e.preventDefault();
+
             var data = $('#contactForm').serializeArray();
+            var name = data[0].value;
+            var replyTo = data[1].value;
+            var tel = data[2].value;
+            var message = data[3].value;
 
-            var message = "Name : " + data[0].value + " " +
-                "Reply to : " + data[1].value + " " +
-                "Tel : " + data[2].value + " " +
-                "Message : " + data[3].value
-
-            $.ajax({
-                url: "https://formspree.io/alexandre.peccaud@gmail.com", 
-                method: "POST",
-                data: {message: message},
-                dataType: "json",
-                success: function(resp, status) {
-                    // Show banner
-                    $('#successBanner').fadeTo(500, 1, function() {
-                        $("#successBanner").css("display", "block");
-                    });
-                    setTimeout(function() {
-                        $('#successBanner').fadeTo(500, 0, function() {
-                            $("#successBanner").css("display", "none");
-                        });
-                    }, 3000);
-
-                    // Reset form
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Show banner
-                    $('#failureBanner').fadeTo(500, 1, function() {
-                        $("#failureBanner").css("display", "block");
-                    });
-                    setTimeout(function() {
-                        $('#failureBanner').fadeTo(500, 0, function() {
-                            $("#failureBanner").css("display", "none");
-                        });
-                    }, 3000);
-                }
-            });
+            sendEmail(name, replyTo, tel, message);
         });
     /* //Contact form - Email */
 
@@ -190,83 +165,158 @@ $(function(){
 
 
     /* Translate (i18next) */
+
+        /* ------- INITIALISATION ------- */
+
         var userLang = navigator.language || navigator.userLanguage;  // Get user language
 
         if (userLang === "fr") {
-            i18next
-                .use(i18nextXHRBackend)
-                .init({
-                    lng: 'fr',
-                    fallbackLng: 'en',
-                    backend: {
-                        loadPath: '/locales/{{lng}}.json'
-                    }
-                }, function(err, t) {
-                    jqueryI18next.init(i18next, $);
-                    $('.translate').localize();
-                });
+            $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/France.png');
+            initI18next("fr");
         }
         else {
+            $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/United-Kingdom.png');
+            var mq = window.matchMedia("(min-width: 1px) and (max-width: 576px)");
+            if (!mq.matches) {
+                initTypedText([
+                    "I am a Web developer.",
+                    "I develop your websites.",
+                    "I am a Web designer."
+                ]);
+            }
+            initI18next("en");
+        }
+
+        function initI18next(langCode) {
+            var filePath = "/locales/" + langCode + ".json"
             i18next
                 .use(i18nextXHRBackend)
                 .init({
-                    lng: 'en',
+                    lng: langCode,
                     fallbackLng: 'en',
                     backend: {
-                        loadPath: '/locales/{{lng}}.json'
+                        loadPath: filePath
                     }
                 }, function(err, t) {
                     jqueryI18next.init(i18next, $);
                     $('.translate').localize();
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/France.png');
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('alt', 'Français');
-                    initTyped(["I am a Web developer.", "I develop your websites.", "I am a Web designer."])
                 });
         }
 
-        // Switch language
-        $('.navbar .translate-flag').click(function() {
-            if ($('.navbar-right li .translate-flag img').attr('alt') === 'English') {
-                i18next.changeLanguage("en", function() {
-                    $('.translate').localize();
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/France.png');
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('alt', 'Français');
-                    initTyped(["I am a Web developer.", "I develop your websites.", "I am a Web designer."])
-                });
-            }
-            else if ($('.navbar-right li .translate-flag img').attr('alt') === 'Français') {
-                i18next.changeLanguage("fr", function() {
-                    $('.translate').localize();
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/United-Kingdom.png');
-                    $('.navbar-right li .translate-flag img, header .translate-flag img').attr('alt', 'English');
-                    initTyped(["Je suis un développeur Web.", "Je développe vos sites Web.", "Je suis un Web designer."])
-                });
-            }
-        })
+        /* ------- // INITIALISATION ------- */
+
+        /* ------- SWITCH LANGuAGE ------- */
+
+        $(".switchToEnglish").click(function() {
+            i18next.changeLanguage("en", function() {
+                $('.translate').localize();
+                $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/United-Kingdom.png');
+                var mq = window.matchMedia("(min-width: 1px) and (max-width: 576px)");
+                if (!mq.matches) {
+                    initTypedText([
+                        "I am a Web developer.",
+                        "I develop your websites.",
+                        "I am a Web designer."
+                    ]);
+                }
+            });
+        });
+
+        $(".switchToFrench").click(function() {
+            i18next.changeLanguage("fr", function() {
+                $('.translate').localize();
+                $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/France.png'); // Update displayed flag
+                var mq = window.matchMedia("(min-width: 1px) and (max-width: 576px)");
+                if (!mq.matches) {
+                    initTypedText([
+                        "Je suis un développeur Web.",
+                        "Je développe vos sites Web.",
+                        "Je suis un Web designer."
+                    ]);
+                }
+            });
+        });
+
+        /* ------- // SWITCH LANGuAGE ------- */
+
+
+        // $('.navbar .translate-flag').click(function() {
+        //     if ($('.navbar-right li .translate-flag img').attr('alt') === 'English') {
+        //         i18next.changeLanguage("en", function() {
+        //             $('.translate').localize();
+        //             $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/France.png');
+        //             $('.navbar-right li .translate-flag img, header .translate-flag img').attr('alt', 'Français');
+        //             initTyped(["I am a Web developer.", "I develop your websites.", "I am a Web designer."])
+        //         });
+        //     }
+        //     else if ($('.navbar-right li .translate-flag img').attr('alt') === 'Français') {
+        //         i18next.changeLanguage("fr", function() {
+        //             $('.translate').localize();
+        //             $('.navbar-right li .translate-flag img, header .translate-flag img').attr('src', 'dist/images/flags/United-Kingdom.png');
+        //             $('.navbar-right li .translate-flag img, header .translate-flag img').attr('alt', 'English');
+        //             initTyped(["Je suis un développeur Web.", "Je développe vos sites Web.", "Je suis un Web designer."])
+        //         });
+        //     }
+        // })
+
     /* //Translate */
 });
 
-/* typed.js - animated text */
-function initTyped(strings) {
-    var mq = window.matchMedia("(min-width: 1px) and (max-width: 576px)");
-    if (!mq.matches) {
-        $("header h2 span").typed({
-            strings: strings,
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 10000,
-            showCursor: true,
-            cursorChar: "|",
-            loop: true,
-            // call when done callback function
-            callback: function() {},
-            // starting callback function before each string
-            preStringTyped: function() {},
-            //callback for every typed string
-            onStringTyped: function() {},
-            // callback for reset
-            resetCallback: function() {}
-        });
-    }
+function initTypedText(stringsArray) {
+    $("header h2 span").typed({
+        strings: stringsArray,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 10000,
+        showCursor: true,
+        cursorChar: "|",
+        loop: true,
+        // call when done callback function
+        callback: function() {},
+        // starting callback function before each string
+        preStringTyped: function() {},
+        //callback for every typed string
+        onStringTyped: function() {},
+        // callback for reset
+        resetCallback: function() {}
+    });
 }
-/* //typed.js -- animated text */
+
+function sendEmail(name, replyTo, tel, message) {
+    var message = "Name : " + name + " " +
+        "Reply to : " + replyTo + " " +
+        "Tel : " + tel + " " +
+        "Message : " + message;
+
+    $.ajax({
+        url: "https://formspree.io/alexandre.peccaud@gmail.com", 
+        method: "POST",
+        data: {message: message},
+        dataType: "json",
+        success: function(resp, status) {
+            // Show banner
+            $('#successBanner').fadeTo(500, 1, function() {
+                $("#successBanner").css("display", "block");
+            });
+            setTimeout(function() {
+                $('#successBanner').fadeTo(500, 0, function() {
+                    $("#successBanner").css("display", "none");
+                });
+            }, 3000);
+
+            // Reset form
+            $('#contactForm').trigger("reset");
+        },
+        error: function() {
+            // Show banner
+            $('#failureBanner').fadeTo(500, 1, function() {
+                $("#failureBanner").css("display", "block");
+            });
+            setTimeout(function() {
+                $('#failureBanner').fadeTo(500, 0, function() {
+                    $("#failureBanner").css("display", "none");
+                });
+            }, 3000);
+        }
+    });
+}
